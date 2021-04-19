e342e8db64d89a4d1e89cec6cc3365899af5ece1
leandro.sales <leandro.sales@ext.webedia-group.com>
2021-04-19 10:28:04 -0300

    ajuste router

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Usuarios from '../views/usuarios/Usuarios.vue'
-import UsuarioForm from '../views/usuarios/UsuarioForm.vue'
+
 
 Vue.use(VueRouter)
 
@@ -9,20 +8,17 @@ const routes = [
   {
     path: '/',
     name: 'Usuarios',
-    component: Usuarios
+    component: () => import(/* webpackChunkName: "about" */ '../views/usuarios/Usuarios.vue')
   },
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
   {
     path: '/cadastrar',
     name: 'UsuarioForm',
-    component: UsuarioForm,
+    component: () => import(/* webpackChunkName: "about" */ '../views/usuarios/UsuarioForm.vue'),
     props: true,
   },
 ]
